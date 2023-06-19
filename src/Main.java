70e43e24972769bf51ad53972716e6d69b1d4848
dvvvelop <dvvvelop@mail.ru>
2023-06-19 18:59:21 +0300

    MainCommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CurrencyConverter converter = new CurrencyConverter("rates.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите выражение: ");
+        String input = scanner.nextLine();
+
+        CurrencyExpression expression = new CurrencyExpression(converter, input);
+        CurrencyValue result = expression.evaluate();
+
+        System.out.println("Результат: " + result.format());
     }
 }
