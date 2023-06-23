3118bb2e7306d80f8a0bbac867c2cec34572d309
dvvvelop <dvvvelop@mail.ru>
2023-06-23 22:19:59 +0300

    Commit10

--- src/CurrencyCalculator.java
+++ src/CurrencyCalculator.java
@@ -19,30 +19,80 @@ public class CurrencyCalculator {
     }
 
     private String evaluateExpression(String input) {
-        input = input.replaceAll("\\s", ""); // Remove whitespace
+        input = input.replaceAll("\\s", ""); // Удаление пробелов
 
-        if (input.startsWith("toRubles(") && input.endsWith(")")) {
+        if (input.startsWith("$") && input.contains("to")) {
+            // Операция со сложением/вычитанием и конвертацией
+            String[] parts = input.split("[+\\-]");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("Некорректная операция");
+            }
+
+            Currency operand1 = parseCurrency(parts[0]);
+            Currency operand2 = parseCurrency(parts[1]);
+
+            // Проверка, является ли операнд конвертацией
+            if (operand1.getCurrencyType().equals("$") && parts[1].startsWith("to")) {
+                operand2 = parseCurrency(evaluateExpression(parts[1])); // Выполнение конвертации
+            } else if (operand2.getCurrencyType().equals("$") && parts[0].startsWith("to")) {
+                operand1 = parseCurrency(evaluateExpression(parts[0])); // Выполнение конвертации
+            }
+
+            Currency result;
+            if (input.contains("+")) {
+                result = operand1.add(operand2);
+            } else {
+                result = operand1.subtract(operand2);
+            }
+
+            return result.toString();
+        } else if (input.startsWith("toRubles(") && input.endsWith(")")) {
+            // Операция конвертации в рубли
             String value = input.substring(9, input.length() - 1);
             return convertToRubles(value);
         } else if (input.startsWith("toDollars(") && input.endsWith(")")) {
+            // Операция конвертации в доллары
             String value = input.substring(10, input.length() - 1);
             return convertToDollars(value);
         } else {
-            return performOperation(input);
+            throw new IllegalArgumentException("Некорректная операция");
         }
     }
 
+
+
     private String convertToRubles(String value) {
-        Currency dollars = parseCurrency(value);
-        double rubles = currencyConverter.toRubles(dollars.getAmount());
-        return new Currency(rubles, "p").toString();
+        try {
+            Currency currency = parseCurrency(value);
+            double rubles;
+            if (currency.getCurrencyType().equals("$")) {
+                rubles = currencyConverter.toRubles(currency.getAmount());
+            } else {
+                throw new IllegalArgumentException("Некорректный формат валюты");
+            }
+            return new Currency(rubles, "p").toString();
+        } catch (IllegalArgumentException e) {
+            return "Ошибка: " + e.getMessage();
+        }
     }
 
     private String convertToDollars(String value) {
-        Currency rubles = parseCurrency(value);
-        double dollars = currencyConverter.toDollars(rubles.getAmount());
-        return new Currency(dollars, "$").toString();
+        try {
+            Currency currency = parseCurrency(value);
+            double dollars;
+            if (currency.getCurrencyType().equals("p")) {
+                dollars = currencyConverter.toDollars(currency.getAmount());
+            } else {
+                throw new IllegalArgumentException("Некорректный формат валюты");
+            }
+            return new Currency(dollars, "$").toString();
+        } catch (IllegalArgumentException e) {
+            return "Ошибка: " + e.getMessage();
+        }
     }
+
+
+
 
 
     private String performOperation(String input) {
@@ -53,6 +103,13 @@ public class CurrencyCalculator {
 
         Currency operand1 = parseCurrency(parts[0]);
         Currency operand2 = parseCurrency(parts[1]);
+
+        // Проверка, является ли операнд конвертацией
+        if (operand1.getCurrencyType().equals("$") && parts[1].startsWith("to")) {
+            operand2 = parseCurrency(evaluateExpression(parts[1])); // Выполнение конвертации
+        } else if (operand2.getCurrencyType().equals("$") && parts[0].startsWith("to")) {
+            operand1 = parseCurrency(evaluateExpression(parts[0])); // Выполнение конвертации
+        }
 
         Currency result;
         if (input.contains("+")) {
@@ -66,8 +123,13 @@ public class CurrencyCalculator {
         return result.toString();
     }
 
+
+
     private Currency parseCurrency(String input) {
         input = input.trim();
+
+        // Replace commas with periods
+        input = input.replace(",", ".");
 
         if (input.startsWith("$")) {
             double amount = Double.parseDouble(input.substring(1));
@@ -75,15 +137,52 @@ public class CurrencyCalculator {
         } else if (input.endsWith("p")) {
             double amount = Double.parseDouble(input.substring(0, input.length() - 1));
             return new Currency(amount, "p");
+        } else if (input.startsWith("toDollars(") && input.endsWith(")")) {
+            String value = input.substring(10, input.length() - 1);
+            double amount = currencyConverter.toDollars(parseCurrency(value).getAmount());
+            return new Currency(amount, "$");
+        } else if (input.startsWith("toRubles(") && input.endsWith(")")) {
+            String value = input.substring(9, input.length() - 1);
+            double amount = currencyConverter.toRubles(parseCurrency(value).getAmount());
+            return new Currency(amount, "p");
         } else {
             throw new IllegalArgumentException("Некорректный формат валюты");
         }
     }
 
 
-    public static void main(String[] args) {
-        CurrencyCalculator calculator = new CurrencyCalculator();
-        calculator.run();
+
+    private double parseAmount(String amountStr) {
+        try {
+            return Double.parseDouble(amountStr.replace(",", ".")); // Заменяем запятую на точку в числе
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Некорректный формат числа");
+        }
     }
+
+
+
+
+    private Currency addCurrencies(Currency operand1, Currency operand2) {
+        if (!operand1.getCurrencyType().equals(operand2.getCurrencyType())) {
+            throw new IllegalArgumentException("Некорректная операция сложения валютных значений: неподдерживаемые типы валют");
+        }
+
+        double resultAmount = operand1.getAmount() + operand2.getAmount();
+        return new Currency(resultAmount, operand1.getCurrencyType());
+    }
+
+    private Currency subtractCurrencies(Currency operand1, Currency operand2) {
+        if (!operand1.getCurrencyType().equals(operand2.getCurrencyType())) {
+            throw new IllegalArgumentException("Некорректная операция вычитания валютных значений: неподдерживаемые типы валют");
+        }
+
+        double resultAmount = operand1.getAmount() - operand2.getAmount();
+        return new Currency(resultAmount, operand1.getCurrencyType());
+    }
+
+
+
 }
+
 
